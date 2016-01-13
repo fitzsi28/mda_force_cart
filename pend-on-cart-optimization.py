a6252d81dc1f0776934a454742ddd0254e99da36
fitzsi28 <fitzsi28@gmail.com>
2016-01-13 16:41:28 -0600

    tuning traj_opt feedback control

--- pend-on-cart-optimization.py
+++ pend-on-cart-optimization.py
@@ -4,18 +4,11 @@ import sys
 import trep
 from trep import tx, ty, tz, rx, ry, rz
 import trep.discopt as discopt
-#import math
-#from math import sin, cos
-#from math import pi as mpi
-#import trep.visual as visual
-#from PyQt4.QtCore import Qt, QRectF, QPointF
-#from PyQt4.QtGui import QColor
 import matplotlib.pyplot as plt
-#import traj_opt as to
 import max_demon as mda
 from max_demon import traj_opt as to
 from max_demon.constants import *
-HORIZ=1.2
+HORIZ=1.3
 
 # Build cart system with torque input on pendulum.
 system = mda.build_system(True)
@@ -28,7 +21,7 @@ mvi_b = trep.MidpointVI(system_b)
 dsys_b = discopt.DSystem(mvi_b, t)
 
 # Generate an initial trajectory
-q0 = mda.generate_initial_trajectory(system, t, 0.3,0.)
+q0,dq0d,dq0k = mda.generate_initial_trajectory(system, t, -0.3)
 (X,U) = dsys_a.build_trajectory(q0)
 for k in range(dsys_a.kf()):  
     if k == 0:
@@ -46,20 +39,18 @@ Rcost = to.make_input_cost(system, dsys_a, 0.3, 0.3, 0.3)
 finished, X,U = to.to_sol(system,t,dsys_a, dsys_b,X,U,Xd,Ud,Qcost,Rcost)
 
 tcurr =0
-tf = 4*HORIZ
-i=0;
-X1=X
+tf = 10
+X1=X[0:12]
 U1=U
 while tcurr<tf:
-    tcurr = tcurr+HORIZ
-    i+=1
+    tcurr = tcurr+TS#HORIZ
     t = np.arange(tcurr, tcurr+HORIZ, DT)
     dsys_a = discopt.DSystem(mvi, t)#can this take short time intervals like 1/5? yes!
     dsys_b = discopt.DSystem(mvi_b, t)
     qd = mda.generate_desired_trajectory(system, t,0.)
     # Generate an initial trajectory
-    q0 = mda.generate_initial_trajectory(system, t, X1[-1,0],X1[-1,1])
-    (X,U) = dsys_a.build_trajectory(q0)
+    q0,dq0d,dq0k = mda.generate_initial_trajectory(system, t, X1[-1,0],X1[-1,1],X1[-1,2],X1[-1,3])
+    (X,U) = dsys_a.build_trajectory(q0,dq0d,dq0k)
     for k in range(dsys_a.kf()):  
         if k == 0:
             dsys_a.set(X[k], U[k], 0) #set the current state, input and time of the discrete system
@@ -68,11 +59,10 @@ while tcurr<tf:
         X[k+1] = dsys_a.f() #the next state of the system
     (Xd, Ud) = dsys_a.build_trajectory(qd)
     finished, Xtemp,Utemp = to.to_sol(system,t,dsys_a, dsys_b,X,U,Xd,Ud,Qcost,Rcost)
-    X1=np.vstack([X1,Xtemp])
+    X1=np.vstack([X1,Xtemp[0:12]])
     U1=np.vstack([U1,Utemp])
-    print i
-
-t=np.arange(0.,tcurr+HORIZ+2*DT,DT)
+    
+t=np.linspace(0.,tcurr+TS,len(X1))#np.arange(0.,tcurr+TS,DT)
 if '--novisual' not in sys.argv:
 
     q,p,v,u,rho = dsys_b.split_trajectory(X1, U1)
